11f846000ecf7c4a12526ce3882336df36bb9a2d
enricocarpio <carpioenrico@gmail.com>
2020-12-10 01:09:40 +0800

    updated

---
+++ app/Http/Middleware/isLoggedRestrict.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class isLoggedRestrict
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(auth()->user()) return redirect()->route('login');
+        return $next($request);
+    }
+}
